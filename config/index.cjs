52641fb36d3264fa4f405ee334f8c2e640fb333f
xumu1209 <114580757+xumu1209@users.noreply.github.com>
2022-09-28 19:52:28 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -4,12 +4,14 @@ const USER_CONFIG = {
    * 公众号配置
    */
 
-  // 公众号APP_ID
+  // 公众号APP_IDwxf11a55a014a97e33
+
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
   APP_ID: '',
 
-  // 公众号APP_SECRET
+  // 公众号APP_SECRET801544205cc67865b19513f47bebffea
+
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
   APP_SECRET: '',
@@ -63,17 +65,17 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '老婆0',
+      name: '楠楠',
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: '',
       // 你想对他发送的模板消息的模板ID
       useTemplateId: '',
       // 所在省份
-      province: '广东',
+      province: '陕西',
       // 所在城市
-      city: '肇庆',
+      city: '西安',
       // 新历生日, 仅用作获取星座运势, 格式必须
-      horoscopeDate: '12-27',
+      horoscopeDate: '07-21',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
       horoscopeDateType: '今日',
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
@@ -82,25 +84,25 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '老婆', year: '1996', date: '09-09',
+          type: '*生日', name: '申楠', year: '2004', date: '07-21',
         },
         {
-          type: '节日', name: '结婚纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '结婚纪念日', year: '2030', date: '10-1',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '刘雄辉', year: '2003', date: '12-09',
         },
         {
-          type: '节日', name: '被搭讪纪念日', year: '2021', date: '09-01',
+          type: '节日', name: '相识纪念日', year: '2022', date: '08-21',
         }
       ],
       // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
+        { keyword: 'love_day', date: '2022-08-25' },
         // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+        { keyword: 'marry_day', date: '2030-10-01' },
         // 退伍日
         { keyword: 'ex_day', date: '2022-09-10' }
       ],
@@ -224,7 +226,7 @@ const USER_CONFIG = {
   CALLBACK_USERS: [
     {
       // 一般都填自己
-      name: '自己',
+      name: '徐暮',
       // 自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: '',
     }
@@ -239,9 +241,9 @@ const USER_CONFIG = {
   /** 天气相关 */
 
   // 默认所在省份, USERS 中没填的话, 会默认拿这里的省份
-  PROVINCE: '广东',
+  PROVINCE: '陕西',
   // 默认所在城市, USERS 中没填的话, 会默认拿这里的城市
-  CITY: '惠州',
+  CITY: '西安',
 
   /** 重要节日相关 */
 
@@ -265,17 +267,17 @@ const USER_CONFIG = {
   FESTIVALS: [
     // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
     {
-      type: '*生日', name: '老婆', year: '1996', date: '09-09', isShowAge: true,
+      type: '*生日', name: '申楠', year: '2004', date: '07-21', isShowAge: true,
     },
     {
-      type: '节日', name: '结婚纪念日', year: '2020', date: '09-03',
+      type: '节日', name: '结婚纪念日', year: '2030', date: 10-01',
     },
     // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
     {
-      type: '生日', name: '李四', year: '1996', date: '09-31', isShowAge: true,
+      type: '生日', name: '刘雄辉', year: '2003', date: '12-09', isShowAge: true,
     },
     {
-      type: '节日', name: '被搭讪纪念日', year: '2021', date: '09-01',
+      type: '节日', name:'相识纪念日', year: '2022', date: '08-21',
     }
     // 你可以不断按格式往下增加
     // ...
@@ -296,9 +298,9 @@ const USER_CONFIG = {
    * */
   CUSTOMIZED_DATE_LIST: [
     // 在一起的日子
-    { keyword: 'love_day', date: '2022-09-08' },
+    { keyword: 'love_day', date: '2022-08-25' },
     // 结婚纪念日
-    { keyword: 'marry_day', date: '2022-09-09' },
+    { keyword: 'marry_day', date: '2030-10-01' },
     // 退伍日
     { keyword: 'ex_day', date: '2022-09-10' }
     // 你可以不断按格式往下增加
